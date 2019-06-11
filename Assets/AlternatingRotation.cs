deb8c10005d2d4dba37fd3d8914fb5ea7bee8f62
joeycarlos <jclaguio@gmail.com>
2019-06-11 13:36:31 -0700

    Add background and boss art

---
+++ Assets/AlternatingRotation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlternatingRotation : MonoBehaviour {
+
+    public float rotationSpeed;
+    public float timeUntilDirectionSwitch;
+
+    private bool rotateCW;
+
+    private float rotationTimer;
+
+	// Use this for initialization
+	void Start () {
+        rotateCW = true;
+        rotationTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdateTimer();
+
+        if (rotationTimer > timeUntilDirectionSwitch)
+            SwitchDirection();
+
+        Rotate();
+	}
+
+    void UpdateTimer()
+    {
+        rotationTimer += Time.deltaTime;
+    }
+
+    void SwitchDirection()
+    {
+        rotateCW = !rotateCW;
+        rotationTimer = 0;
+    }
+
+    void Rotate()
+    {
+        if (rotateCW == true)
+            transform.Rotate(Vector3.forward, - rotationSpeed * Time.deltaTime);
+        else 
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
